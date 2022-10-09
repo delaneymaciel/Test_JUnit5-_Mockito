1b9a368a086546774707d9d9d03e9e74d12e5549
Delaney Maciel <delaneymaciel@hotmail.com>
2022-10-09 11:25:13 -0300

    Endpoint create para user

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -5,11 +5,10 @@ import br.com.dicasdeumdev.api.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,5 +36,13 @@ public class UserResource {
                 .body(service.findAll()
                         .stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
     }
-
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj){
+        /*
+        As duas linhas viraram uma só (comentário só para didática)
+        User newObj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(newObj.getId()).toUri();*/
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(service.create(obj).getId()).toUri();
+        return  ResponseEntity.created(uri).build();
+    }
 }
