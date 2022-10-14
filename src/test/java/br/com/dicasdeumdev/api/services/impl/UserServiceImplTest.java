1a4074003ea91c70fa15c5257b63d6099836fa08
Delaney Maciel <delaneymaciel@hotmail.com>
2022-10-13 22:38:24 -0300

    Teste para a classe UserService no metodo findByID objeto não encontrado.

--- src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package br.com.dicasdeumdev.api.services.impl;
 import br.com.dicasdeumdev.api.domain.User;
 import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.repositories.UserRepository;
+import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -56,6 +57,18 @@ class UserServiceImplTest {
         assertEquals(EMAIL,response.getEmail());
     }
 
+    @Test
+    void whenFindByIdThenReturnAnObjectNotFoundException() {
+        when(repository.findById(Mockito.anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontrado."));
+
+        try{
+            service.findById(ID);
+        } catch (Exception ex){
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals("Objeto não encontrado.", ex.getMessage());
+        }
+
+    }
     @Test
     void findAll() {
     }
